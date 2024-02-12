20c914de043080ee31ad3025199f7cdec3626258
nirajjha3 <nirajjha3110@gmail.com>
2024-02-12 21:11:52 +0530

    Added the updated JavaScript File

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 const prices = document.querySelectorAll(".main-input");
 const calculateBtn = document.querySelector(".analyseBtn");
 const dialogBox = document.getElementById("dialog-box");
-const showcase = document.getElementById("showcase");
+
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const message = document.getElementById("message");
 
 calculateBtn.addEventListener("click", calculateStocks);
 
+// Remember always to take input in the function, if not done might lead to logical error.
+// And try to parse the value of the input so that the string value to the int value, so that it is easy to calculate.
 function calculateStocks() {
-    const purchasePrice = document.getElementById("purchasePrice").value;
-    const stocks = document.getElementById("stocks").value;
-    const currentPrice = document.getElementById("currentPrice").value;
-    
-    const title = document.getElementById("title");
-    const description = document.getElementById("description");
-    const message = document.getElementById("message");
+    const purchasePrice = parseFloat(document.getElementById("purchasePrice").value);
+    const stocks = parseFloat(document.getElementById("stocks").value);
+    const currentPrice = parseFloat(document.getElementById("currentPrice").value);
 
-    if(purchasePrice == currentPrice) {
+    if(purchasePrice == 0 || stocks == 0 || currentPrice == 0) {
+        title.innerHTML = "Fill in valid inputs!!!";
+        description.innerText = "";
+        message.innerText = "";
+    } else if(purchasePrice == currentPrice) {
         title.innerHTML = "Bhai tera nasib hi kharab hai!!!";
-    } else if(purchasePrice < currentPrice) {
-        let profit = currentPrice - purchasePrice;
-        let profitPercent = profit / 100;
-        title.innerHTML = "Bhai, Kamal kardiya!!!";
-        description.innerHTML = "Total Profit: ${profit} <br> Profit Percentage: ${profitPercent}";
-        message.innerHTML = "Bhai party kab de raha hai";
-    } else {
-        let loss = purchasePrice - currentPrice;
-        let lossPercent = loss / 100;
+        description.innerText = "";
+        message.innerText = "";
+    } else if(purchasePrice * stocks > currentPrice * stocks) {
+        let loss = Math.abs(purchasePrice * stocks - currentPrice * stocks);
+        let lossPercent = loss / (purchasePrice * stocks) * 100;
         title.innerHTML = "Bhai, tera katgaya!!!";
-        description.innerHTML = "Total Loss: ${loss} <br> Loss Percentage: ${lossPercent}";
-        message.innerHTML = "Koi na bhai, lage raho!!!";
+        description.innerText = `Total Loss: ${loss} Loss Percentage: ${lossPercent}%`;
+        message.innerText = "Koi na bhai, lage raho!!!";
+    } else if(purchasePrice * stocks < currentPrice * stocks) {
+        let profit = currentPrice * stocks - purchasePrice * stocks;
+        let profitPercent = profit / (purchasePrice * stocks) * 100;
+        title.innerHTML = "Bhai, Kamal kardiya!!!";
+        description.innerText = `Total Profit: ${profit} Profit Percentage: ${profitPercent}%`;
+        message.innerText = "Bhai party kab de raha hai";
     }
 }
 
